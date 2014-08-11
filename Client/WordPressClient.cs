074ed7abe1315a3ec1a83706ba8b55983296a5eb
msiccdev <msiccdev@hotmail.com>
2014-08-11 07:37:32 +0200

    first upload to save the project

---
+++ Client/WordPressClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordPressUniversal.Client
+{
+    public class WordPressClient
+    {
+        /// <summary>
+        /// your site url
+        /// to be set without 'http://' prefix
+        /// </summary>
+        public string siteUrl { get; private set; }
+
+
+
+
+
+
+
+    }
+}
---
+++ Client/endPoints.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordPressUniversal.Models;
+
+namespace WordPressUniversal.Client
+{
+    public class EndPoints
+    {
+
+        public string postsUrl(string site, PostType type, PostStatus status, int? number = null)
+        {
+            int postNumber = 10;
+            if (number != null)
+            {
+                postNumber = Convert.ToInt32(number);
+            }
+            var postType = Enum.GetName(typeof(PostType), type).ToLower();
+
+            var postStatus = Enum.GetName(typeof(PostStatus), status).ToLower();
+
+            return string.Format("https://public-api.wordpress.com/rest/v1/sites/{0}/posts/?number={1}&type={2}&status={3}", site, number, postType, postStatus);
+                              
+        }
+    }
+}
